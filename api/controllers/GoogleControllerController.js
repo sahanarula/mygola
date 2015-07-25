7e2f8a73721f00ffb45dfa3fff53c3428efae777
sahanarula <sahiln123@gmail.com>
2015-07-25 16:30:37 +0530

    minor changes

--- api/controllers/GoogleControllerController.js
+++ api/controllers/GoogleControllerController.js
@@ -8,10 +8,9 @@ module.exports = {
 	index: function(req, res){
 		lat = req.param('lat');
 		lon = req.param('lon');
-		console.log(lat);
 		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat +','+ lon +'&radius=500&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		  	console.log(body["results"]);
+		  	console.log(response.body);
 		  	res.json(body["results"]) // Show the HTML for the Google homepage.
 		  }
 		});
