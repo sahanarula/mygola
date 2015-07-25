6c86955eaef0901c47e4310cdd3a9c8dca065ecf
sahanarula <sahiln123@gmail.com>
2015-07-25 15:57:21 +0530

    responding with json data

--- api/controllers/GoogleControllerController.js
+++ api/controllers/GoogleControllerController.js
@@ -6,12 +6,11 @@
  */
 module.exports = {
 	index: function(req, res){
-		lat = req.param('lat');
-		lon = req.param('lon');
-		console.log(lat);
-		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat +','+ lon +'&radius=500&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
+		console.log(req.param('lat'));
+		console.log(req.param('long'));
+		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9123564,77.64214390000006&radius=500&types=food&name=cruise&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
-		    console.log(body) // Show the HTML for the Google homepage.
+		  	res.json(body);
 		  }
 		});
 	}	
