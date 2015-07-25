6b5e4eeafe54dab19160c94ee11ccb0c048a75ac
sahanarula <sahiln123@gmail.com>
2015-07-25 15:31:36 +0530

    added lat lon variable

--- api/controllers/GoogleControllerController.js
+++ api/controllers/GoogleControllerController.js
@@ -6,8 +6,10 @@
  */
 module.exports = {
 	index: function(req, res){
-		console.log(sails.conf.api_key);
-		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9123564,77.64214390000006&radius=500&types=food&name=cruise&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
+		lat = req.param('lat');
+		lon = req.param('lon');
+		console.log(lat);
+		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat +','+ lon +'&radius=500&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		    console.log(body) // Show the HTML for the Google homepage.
 		  }
