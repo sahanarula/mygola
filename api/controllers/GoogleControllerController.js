c736480298b2c197f9758275904a75aafce9e45b
sahanarula <sahiln123@gmail.com>
2015-07-25 15:23:52 +0530

    updated bangalore cords

--- api/controllers/GoogleControllerController.js
+++ api/controllers/GoogleControllerController.js
@@ -7,7 +7,7 @@
 module.exports = {
 	index: function(req, res){
 		console.log(sails.conf.api_key);
-		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=77.64214390000006,12.9123564&radius=500&types=food&name=cruise&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
+		sails.request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=12.9123564,77.64214390000006&radius=500&types=food&name=cruise&key=' + sails.conf.api_key + '&sensor=true', function (error, response, body) {
 		  if (!error && response.statusCode == 200) {
 		    console.log(body) // Show the HTML for the Google homepage.
 		  }
