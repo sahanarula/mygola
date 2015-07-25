723384db125131d434fe30f0ade7607687aa7f11
sahanarula <sahiln123@gmail.com>
2015-07-25 21:42:10 +0530

    parsing json

--- api/controllers/GoogleControllerController.js
+++ api/controllers/GoogleControllerController.js
@@ -48,7 +48,7 @@ module.exports = {
 			var url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ lat +','+ lon +'&radius=500&types=' + q[i] + '&key=' + sails.conf.api_key + '&sensor=true';
 			counts = 0;
 			var response = sails.requestSync('GET', url);
-			var result = response.body.toString('utf8');
+			var result = JSON.parse(response.body.toString('utf8'));
 			console.log(result);
 			res.json(result);
 		}	
